7a57ec56e9e1849640a9c057bd5c8bcb7f947d57
Rohan Woodley <rohan@sprynet.com>
2015-06-07 11:39:56 -0500

    Updated label

--- plot2.R
+++ plot2.R
@@ -22,7 +22,7 @@ plot2 <- function() {
   # Open Device name plot2.png"
   png("plot2.png")
   
-  plot(hpcsub$DateTime, hpcsub$Global_active_power, type='l', ylab="Global Activet Power (kilowatts", xlab="")
+  plot(hpcsub$DateTime, hpcsub$Global_active_power, type='l', ylab="Global Active Power (kilowatts", xlab="")
   
   # Turn Device Off
   dev.off()
